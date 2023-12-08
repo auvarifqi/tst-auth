9d32c78bdf010c5f05c73dd332b24870af455c0e
auvarifqi <88789984+auvarifqi@users.noreply.github.com>
2023-12-08 22:26:08 +0700

    put cors

--- dockerfile
+++ dockerfile
@@ -8,3 +8,6 @@ RUN pip install -r requirements.txt
 
 # Command to run the FastAPI server when the container starts
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from routes.requirements import archive_router, verify_router, transaction_router
 from routes.auth import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Setelan CORS untuk menerima permintaan dari semua domain
+origins = ["*"]
+
+# Tambahkan middleware CORS ke aplikasi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)  
 app.include_router(archive_router, prefix ="/archive")
 app.include_router(verify_router, prefix ="/verify")
 app.include_router(transaction_router, prefix ="/transaction")
+
